29405267f83ea92ba86de35882128136717ec636
dilan23Jill <muzicdilan@gmail.com>
2021-12-31 13:02:17 +0100

    fix1

--- NikeTV/js/script.js
+++ NikeTV/js/script.js
@@ -148,3 +148,15 @@ function animationOff(){
      document.getElementById('moznost1').classList.add('backgroundOn');
     }, 700);}
 
+var c = document.getElementById("BezierCurveCanvas");
+          var context = c.getContext("2d");
+        context.beginPath();
+        context.moveTo(353, 393);
+        context.bezierCurveTo(586, 436, 1044, 198, 1905, 18);
+        context.bezierCurveTo(617, 530, 348, 777, 114, 644);
+        context.bezierCurveTo(-126, 464, 183, 123, 290, 8);
+        context.bezierCurveTo(181, 197, 202, 359, 353, 393);
+        context.fillStyle = "#FF2400";
+        context.strokeStyle = "#FF2400";
+        context.stroke();
+        context.fill();
